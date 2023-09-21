7cafafc4a4998531f1d26a6cdb63c7be2a2fe329
aimlow <aimlow88@daum.net>
2023-09-21 21:47:15 +0900

    9월21일 강의

--- src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
+++ src/main/java/com/quiz/lesson01/Lesson01Quiz03Controller.java
@@ -8,6 +8,9 @@ public class Lesson01Quiz03Controller {
 	
 	@RequestMapping("/lesson01/quiz03/1")
 	public String quiz03() {
+		
+		//@ResponseBody 어노테이션이 없을 때 String을 리턴하면
+		// ViewResolver에 의해 리턴된 String경로의 jsp view화면을 찾고 태그가 Response body에 담긴다.
 		return "lesson01/quiz03";
 	}
 }
